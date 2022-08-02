996d9ee59face31ba95ada49b1100d9ebf5f1b06
Andrey <andreytsuzuki@gmail.com>
2022-08-02 12:01:23 -0300

    setUrlStrategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'package:grafu/app_module.dart';
 import 'package:grafu/app_widget.dart';
@@ -8,6 +9,8 @@ main() {
   Modular.to.addListener(() {
     debugPrint(Modular.to.path);
   });
+
+  setUrlStrategy(PathUrlStrategy());
 
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
